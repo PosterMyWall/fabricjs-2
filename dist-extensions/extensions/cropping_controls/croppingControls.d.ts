b74a2dd696968c5192b1812b2fb92b54f42d9838
Azmeer <azmeer@250mils.com>
2026-05-08 17:21:47 +0500

    force-add dist-extensions for git-url install

---
+++ dist-extensions/extensions/cropping_controls/croppingControls.d.ts
@@ -0,0 +1,12 @@
+import { Control } from '@postermywall/fabricjs-2';
+export declare const createImageCroppingControls: () => {
+    ml: Control;
+    mr: Control;
+    mb: Control;
+    mt: Control;
+    tl: Control;
+    tr: Control;
+    bl: Control;
+    br: Control;
+};
+//# sourceMappingURL=croppingControls.d.ts.map
